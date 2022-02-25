5acb48bf2cbdd62ad74860edbf604eb6f8bba51c
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-02-25 20:42:50 +0700

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,10 +2,10 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n'
-                    + 'FN:Vlynn\n' // full name
-                    + 'TEL;type=CELL;type=VOICE;waid=6281368971807:+62 813-6897-1807\n' // WhatsApp ID + phone number
+                    + 'FN:Riyoshi\n' // full name
+                    + 'TEL;type=CELL;type=VOICE;waid=6285664770042:+62 856-6477-0042\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Vlynn', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Riyoshi', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
