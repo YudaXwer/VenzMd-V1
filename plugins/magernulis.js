7e0eda55a62f8d82bfc1df80a30fef7bd7be7dc7
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-02-27 10:03:54 +0700

    update magernulis.js

--- plugins/magernulis.js
+++ plugins/magernulis.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Uhm... Teksnya?', m)
   await m.reply('Sedang membuat...')
- let hasil = await (await fetch('https://hadi-api.herokuapp.com/api/canvas/nulis?text=${text}').buffer())
+ let hasil = await (await fetch('https://hadi-api.herokuapp.com/api/canvas/nulis?text=${text}')).buffer()
  let caption = `*Nih kak:)*`
     conn.sendFile(m.chat, hasil, '', '*Nih Kak:)*', m, 0, { thumbnail: Buffer.alloc(0) })
 }
