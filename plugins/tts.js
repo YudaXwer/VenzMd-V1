7719ea63039ec7d9111b0aa8fcfd83cb763a7a43
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-02-27 09:24:54 +0700

    update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -11,7 +11,7 @@ let handler = async (m, {conn, text}) => {
 handler.command = ['tts']
 handler.help = ['tts']
 handler.tags = ['tools']
-
+handler.limit = true
 module.exports = handler
 function tts(text) {
     return new Promise((resolve, reject) => {
