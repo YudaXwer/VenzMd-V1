d599689ef0a6a5d49c0787975d6873ef6aa5d915
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-02-27 09:15:03 +0700

    update magernulis.js

--- plugins/magernulis.js
+++ plugins/magernulis.js
@@ -1,18 +1,23 @@
 let fetch = require('node-fetch')
-let handler = async (m, { command, conn, text }) => {
-  let id = (command.match(/[1-6]$/) || [])[0] || ''
-  let url = global.API('xteam', '/magernulis' + id, {
-    text,
-    nama: conn.getName(m.sender),
-    kelas: ' '
-  }, 'APIKEY')
-  await conn.sendFile(m.chat, url, 'nulis.jpg', '', m, false, { thumbnail: await (await fetch(url)).buffer() })
+let handler = async(m, { conn, text }) => {
+    if (!teks) return conn.reply(m.chat, 'Uhm... Teksnya?', m)
+  await m.reply('Sedang membuat...')
+ let hasil = await (await fetch('.fetch http://hadi-api.herokuapp.com/api/canvas/nulis?text=${teks}')).buffer()
+ let caption = `*Nih kak:)*`
+
+    conn.sendFile(m.chat, hasil, 'magernulis.jpg', caption, m)
 }
-handler.help = new Array(6).fill('magernulis').map((v, i) => v + (i + 1) + ' <teks>')
+handler.help = ['magernulis <teks>']
 handler.tags = ['nulis']
+handler.command = ['magernulis']
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-handler.command = /^magernulis[1-6]?$/i
-
+handler.fail = null
+handler.exp = 0
 handler.limit = true
 
 module.exports = handler
