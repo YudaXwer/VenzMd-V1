154acdc99db1141b363754e49c54d1d76227b898
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-02-27 13:38:48 +0700

    Add files via upload

---
+++ plugins/logo3.js
@@ -0,0 +1,22 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, '*Teksnya Mana Kak ?*', m)
+  await m.reply('Sedang membuat...')
+ let hasil = await (await fetch(`https://yx-api.herokuapp.com/api/canvas/gfx2?nama=${text}`)).buffer()
+ let caption = `*Nih kak:)*`
+    conn.sendFile(m.chat, hasil, '', '*Nih Kak, satu logo 1k:v*', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['logo3 <teks>']
+handler.tags = ['logo']
+handler.command = ['logo3']
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
+
+module.exports = handler
