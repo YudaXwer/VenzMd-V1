a5d43470decca60b719a58b39ecc3c87fbb5e17f
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-03-03 11:02:36 +0700

    update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 m.reply(wait)
-heum = await fetch(`https://xteam.xyz/randomimage/hentai?APIKEY=KurrXd`)
+heum = await fetch(`https://xteam.xyz/randomimage/hentai?APIKEY=Cristian9407`)
    if (!heum) throw json
     json = await heum.buffer()
-   conn.sendFile(m.chat, json, 'Uwoghh Segss', wm, 'NEXT', `${usedPrefix + command}`, m)
+   conn.sendFile(m.chat, json, 'Uwoghhh Segsss:D', wm, 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['hentai']
 handler.tags = ['hentai']
