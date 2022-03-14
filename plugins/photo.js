fe7eaa6b13482356a3aeec404076ba047c89b4cc
Mengrio <85853927+Mengrio@users.noreply.github.com>
2022-03-14 23:31:33 +0700

    update photo.js

--- plugins/photo.js
+++ plugins/photo.js
@@ -3,13 +3,15 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
 let [teks1, teks2] = text.split `|`
 
-let listeffect = `*Teks Satunya Harap Diisi !*`
-    if (!teks1) return conn.reply(m.chat, listeffect, m)
+let textinv = `*Teks Satunya Harap Diisi !*`
+let teksinv = `*Teks Duanya Harap Diisi !*`
+    if (!teks1) return conn.reply(m.chat, textinv, m)
+    if (!teks2) return conn.reply(m.chat, teksinv, m)
     if (!text) return conn.reply(m.chat, 'Teksnya Mana kak ?', m)
 
   await m.reply('Sedang membuat...')
  let hasil = await (await fetch('https://yx-api.herokuapp.com/api/canvas/gfx3' + '?text1=' + teks1 + '&text2=' + teks2)).buffer()
- let caption = `*Nih Kak, Satu Logo 1k:v`
+ let caption = `*Nih Kak, Satu Logo 1k:v*`
 
     conn.sendFile(m.chat, hasil, 'logo4.jpg', caption, m)
 }
